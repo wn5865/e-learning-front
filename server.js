115cddd8e4e3a4af1a684f08ca3945a83ebafae7
wn5865 <wn2262@gmail.com>
2022-03-22 11:37:52 +0900

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require("express");
-const path = require("path");
 const app = express();
-app.use(express.static(__dirname + "/dist/e-learning-demo"));
+app.use(express.static("./dist/01-frontend"));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/e-learning-demo/index.html"));
+  res.sendFile("index.html", { root: "dist/01-frontend/" });
 });
 app.listen(process.env.PORT || 8080);
