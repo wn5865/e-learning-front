691c425cd55eb1523b3fbbd9dd45dc9f7581f5c8
wn5865 <wn2262@gmail.com>
2022-03-22 16:01:44 +0900

    revised login callback address

--- src/app/config/app-config.ts
+++ src/app/config/app-config.ts
@@ -2,9 +2,10 @@ export default {
   oidc: {
     clientId: '0oa466whh4asAEs7q5d7',
     issuer: 'https://dev-41768260.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:4200/login/callback',
+    redirectUri: 'https://jiwon-online-learning.herokuapp.com//login/callback',
     scopes: ['openid', 'profile', 'email'],
   },
   API_URL:
     'http://Onlinelearningdemo-env.eba-mqhr6hhf.us-east-1.elasticbeanstalk.com',
+  HEROKU_URL: 'https://jiwon-online-learning.herokuapp.com/',
 };
